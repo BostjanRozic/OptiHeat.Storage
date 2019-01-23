050eaab3d9ca4d9e0cd911325d71134c0d3a969b
Boštjan Rožič <bostjan.rozic@gmail.com>
2019-01-23 15:50:18 +0100

    implementacija SpecificationServiceControllerja

---
+++ src/main/java/optiheat/storage/controller/SpecificationServiceController.java
@@ -0,0 +1,67 @@
+package optiheat.storage.controller;
+
+import optiheat.storage.model.Room;
+import optiheat.storage.model.Unit;
+import optiheat.storage.model.User;
+import optiheat.storage.service.SpecificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SpecificationServiceController
+{
+    @Autowired
+    SpecificationService specificationService;
+
+    @RequestMapping(value = "/Storage/SpecificationService/createUnit", method = RequestMethod.POST)
+    public void createUser(@RequestBody String userId, @RequestBody Unit unit)
+    {
+        specificationService.createUnit(userId, unit);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/createRoom", method = RequestMethod.POST)
+    public void createRoom(@RequestBody Room room)
+    {
+        specificationService.createRoom(room);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/updateUnit", method = RequestMethod.PATCH)
+    public void updateUser(@RequestBody Unit unit)
+    {
+        specificationService.updateUnit(unit);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/updateRoom", method = RequestMethod.PATCH)
+    public void updateRoom(@RequestBody Room room)
+    {
+        specificationService.updateRoom(room);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/deleteUnit", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteUnit(@RequestParam("unitId") String unitId)
+    {
+        specificationService.deleteUnit(unitId);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/deleteRoom", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteRoom(@RequestParam("roomId") String roomId)
+    {
+        specificationService.deleteRoom(roomId);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/getUnit", method = RequestMethod.GET)
+    @ResponseBody
+    public Unit getUnit(@RequestParam("unitId") String unitId)
+    {
+        return specificationService.getUnit(unitId);
+    }
+
+    @RequestMapping(value = "/Storage/SpecificationService/getRoom", method = RequestMethod.GET)
+    @ResponseBody
+    public Room getRoom(@RequestParam("roomId") String roomId)
+    {
+        return specificationService.getRoom(roomId);
+    }
+}
